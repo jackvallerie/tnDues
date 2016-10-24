81aed222c0df0b5ae80530b3e9d1f45434b95987
jaeteekae <julia.knight@tufts.edu>
2016-10-23 22:02:48 -0400

    moved things around; pages use templating

--- server/views.py
+++ server/views.py
@@ -1,16 +1,25 @@
-from flask import render_template_string
+from flask import render_template_string, render_template
 from config import *
 from flask_user import login_required
 
 @app.route('/')
-@login_required
-def hello_world():
-  return render_template_string("""
-    {% extends "base.html" %}
-    {% block content %}
-        <h2>Members page</h2>
-        <p>This page can only be accessed by authenticated users.</p><br/>
-        <p><a href={{ url_for('user.register') }}>Home page</a> (anyone)</p>
-        <p><a href={{ url_for('user.profile') }}>Members page</a> (login required)</p>
-    {% endblock %}
-    """)
+# @login_required
+def index():
+  # return render_template_string("""
+  #   {% extends "base.html" %}
+  #   {% block content %}
+  #       <h2>Members page</h2>
+  #       <p>This page can only be accessed by authenticated users.</p><br/>
+  #       <p><a href={{ url_for('user.register') }}>Home page</a> (anyone)</p>
+  #       <p><a href={{ url_for('user.profile') }}>Members page</a> (login required)</p>
+  #   {% endblock %}
+  #   """)
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/programs')
+def programs():
+    return render_template('programs.html')
