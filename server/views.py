ba59417f92883a8097b9a0ed30c21ed921e4a086
jaeteekae <julia.knight@tufts.edu>
2016-10-26 21:24:52 -0400

    did some css-y things

--- server/views.py
+++ server/views.py
@@ -1,4 +1,4 @@
-from flask import render_template_string, render_template
+from flask import render_template_string, render_template, request
 from config import *
 from flask_user import login_required
 
