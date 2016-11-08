1fc9b235d9080170f1eaf6b56730b96f7de3b42a
ducnguyentufts <manh.nguyen@tufts.edu>
2016-11-07 21:44:36 -0500

    Added some backend login stuffs

--- server/views.py
+++ server/views.py
@@ -3,7 +3,8 @@ from .config import *
 from flask_user import login_required
 from .models import db
 from .models import *
-import stripe 
+
+import stripe
 
 @app.route('/')
 # @login_required
@@ -37,19 +38,58 @@ def charge():
         #email='talloiresnetwork@tufts.edu',
         source=request.form['stripeToken']
     )
-    
+
     charge = stripe.Charge.create(
         customer=customer.id,
         amount=amount,
         currency='usd',
         description='Flask Charge'
     )
-    
+
     '''
     return render_template('charge.html', amount=amount)
 
 
 
+########### FLASK LOGIN FACILITY ##############
+import flask_login
+login_manager = flask_login.LoginManager()
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.get(user_id)
+
+
+# https://flask-login.readthedocs.io/en/latest/
+# Note that the class User needs to implement a number of methods
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    # Here we use a class of some kind to represent and validate our
+    # client-side form data. For example, WTForms is a library that will
+    # handle this for us, and we use a custom LoginForm to validate.
+    form = LoginForm()
+    if form.validate_on_submit():
+        # Login and validate the user.
+        # user should be an instance of your `User` class
+        login_user(user)
+
+        flask.flash('Logged in successfully.')
+
+        next = flask.request.args.get('next')
+        # next_is_valid should check if the user has valid
+        # permission to access the `next` url
+        if not next_is_valid(next):
+            return flask.abort(400)
+
+        return flask.redirect(next or flask.url_for('index'))
+    return flask.render_template('login.html', form=form)
+
+@app.route("/settings")
+@login_required
+def settings():
+    pass
+########## TESTING PURPOSE ##########
 
 @app.route('/user/sign-in')
 @login_required
